c59e38e3f8979986ce96e9b39f678e0789c9ae88
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-19 21:43:52 +0200

    Calcul de Réduction

---
+++ challange7.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let num1 = Number(prompt("saisissez le prix : "));
+let num2 = Number(prompt("saisissez le pourcentage de réduction : "));
+
+if (num2 < 0 || num2 > 100 ){
+    console.log("immpossible!")
+}
+
+console.log(`le prix final = ${num1 - num2} DH`);
