fbb34fa2a50f86870f8e4ea8f2cdd4168c573ea7
unknown <ouddoucheabdeljalil@gmail.com>
2026-09-19 21:04:24 +0200

    Opérateurs d'Affectation

---
+++ challange3.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+    let num = 10;
+    console.log("nombre = ", num);
+
+    num += 5 ;
+    console.log("+= 5 = " , num);
+    num -= 2;
+    console.log("-= 2 = " , num);
+    num *= 3 ;
+    console.log("*= 3 = " , num);
+    num /= 4 ;
+    console.log(" /= 5 = " , num);
+    
+    
+    
